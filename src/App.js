640eadf7f53748abb1b1207e6915191422276d8b
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2019-12-17 20:35:52 +0530

    Added display code and json parsing

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Landing from "./portfolio/landing";
+import About from "./portfolio/about";
+import Education from "./portfolio/education";
+import Work from "./portfolio/work";
+import Volunteer from "./portfolio/volunteer";
+import Extra from "./portfolio/extra";
+import Contact from "./portfolio/contact";
+
+import './portfolio.css';
+import { profile } from './file.json';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="portfolioContainerFull">
+        <Landing
+          name={profile.user.name}
+          label={profile.about.label}
+        />
+        <About summary={profile.about} />
+        <Education education={profile.education} />
+        <Work work={profile.work} />
+        <Volunteer volunteer={profile.volunteer} />
+        <Extra
+          awards={profile.awards}
+          publications={profile.publications}
+          languages={profile.languages}
+          skills={profile.skills}
+        />
+        <Contact
+          email={profile.user.email}
+          phone={profile.user.phone}
+          location={profile.location}
+        />
+      </div>
   );
 }
 
