fb038431015892878a814084f54d7a31e5891e84
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2019-12-19 23:11:04 +0530

    Updates

---
+++ precache-manifest.34c0b8d3cfa4d76929715e0216f5dcac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a17264e72de2dba744119dcc8da682",
+    "url": "/PortyDisplay/index.html"
+  },
+  {
+    "revision": "53c663c1d13e831948d2",
+    "url": "/PortyDisplay/static/css/main.d6bd6bd4.chunk.css"
+  },
+  {
+    "revision": "a26c0c856a73b6a5f402",
+    "url": "/PortyDisplay/static/js/2.5c64ec35.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/PortyDisplay/static/js/2.5c64ec35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c663c1d13e831948d2",
+    "url": "/PortyDisplay/static/js/main.822e492a.chunk.js"
+  },
+  {
+    "revision": "cd8840b09c7cd5b55498",
+    "url": "/PortyDisplay/static/js/runtime-main.f2766a57.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/PortyDisplay/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/PortyDisplay/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PortyDisplay/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/PortyDisplay/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/PortyDisplay/static/media/skill.756b3183.jpg"
+  }
+]);
