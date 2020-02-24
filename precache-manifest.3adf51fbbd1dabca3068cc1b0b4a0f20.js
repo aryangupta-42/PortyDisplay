9b524082a12dec35ae945badbaf8fd966a6b1a90
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2020-02-24 20:54:57 +0530

    Updates

---
+++ precache-manifest.3adf51fbbd1dabca3068cc1b0b4a0f20.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11ea3f361e605ed1849baf694943bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adcbeef8aed174f8f01c",
+    "url": "/static/css/main.fd22763f.chunk.css"
+  },
+  {
+    "revision": "4383bacfdf88f71f4ec5",
+    "url": "/static/js/2.02e0d1ad.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.02e0d1ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "adcbeef8aed174f8f01c",
+    "url": "/static/js/main.da42eeb6.chunk.js"
+  },
+  {
+    "revision": "ad50f3ee87457d209677",
+    "url": "/static/js/runtime-main.f1596703.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/static/media/skill.756b3183.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PortyDisplay/precache-manifest.e961705e1f0ff96ca16273a41425966f.js"
+  "/precache-manifest.3adf51fbbd1dabca3068cc1b0b4a0f20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortyDisplay/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
